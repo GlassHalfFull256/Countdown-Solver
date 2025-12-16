39e8385e982d7a477834a6f40a2b554f605afd64
James D <James@dodson.uk>
2025-12-16 13:16:31 +0000

    Test

--- main.py
+++ main.py
@@ -64,6 +64,6 @@ def solve(start_numbers, goal):
                     stack.append(Q)
 
 operators = (operator.add, operator.sub, operator.mul, operator.truediv, lambda a, b: b - a, lambda a, b: b/a)
-start_numbers = tuple(map(int, input('Starting numbers: ').split()))
+start_numbers = tuple(map(int, input('Starting numbers (Use \' \' as a delimiter): ').split()))
 goal = int(input('Goal: '))
 solve(start_numbers, goal)
